2b5e8f289843cc9ea010c4f5e5a67faeeee2cdb8
vqcuong <35632030+vqcuong@users.noreply.github.com>
2023-11-05 02:48:25 +0700

    Update

--- auto-ls.plugin.zsh
+++ auto-ls.plugin.zsh
@@ -5,24 +5,24 @@ auto-ls() {
   exa_cmd="exa --icons --color=always --group --time-style=long-iso --group-directories-first"
   colorls_cmd="colorls --group-directories-first"
   ls_cmd="ls"
-  if [ $ZSH_AUTO_LS_LIST_ALL == "true" ]; then
+  if $ZSH_AUTO_LS_LIST_ALL = "true"; then
     exa_cmd="$exa_cmd --all"
     colorls="$colorls_cmd -A"
     ls_cmd="$ls_cmd -A"
   fi
   if command -v exa &>/dev/null; then
-    exec $exa_cmd
+    eval $exa_cmd
     return
   fi
   if command -v $HOMEBREW_PREFIX/bin/exa &>/dev/null; then
-    exec $exa_cmd
+    eval $exa_cmd
     return
   fi
   if command -v colorls &>/dev/null; then
-    exec $colorls_cmd
+    eval $colorls_cmd
     return
   fi
-  exec $ls_cmd
+  eval $ls_cmd
 }
 
 chpwd_functions=(auto-ls $chpwd_functions)
